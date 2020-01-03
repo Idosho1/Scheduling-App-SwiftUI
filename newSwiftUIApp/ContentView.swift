e9acb942eea0d4795057a52642a205ede98b704d
IdoS <ido.shoshani1@gmail.com>
2020-01-03 07:45:26 -0500

    Initial Commit

---
+++ newSwiftUIApp/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  newSwiftUIApp
+//
+//  Created by Ido Shoshani on 1/3/20.
+//  Copyright © 2020 Ido Shoshani. All rights reserved.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, World!")
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
